490e3752b8cffc8e9b42689a41c361bc1c59c0ab
Mete Ozguz <ozguz.mete@gmail.com>
2025-11-13 16:08:39 -0500

    feat: add Config.java

---
+++ src/main/java/com/example/fileorganizer/Config.java
@@ -0,0 +1,8 @@
+package com.example.fileorganizer;
+
+    /**
+     * Placeholder for configuration.
+     */
+    public class Config {
+        public static final String VERSION = "0.1.0";
+    }
